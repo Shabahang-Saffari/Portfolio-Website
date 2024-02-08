5d6e86dfba7a1e228dd91c7f5b483fbafe55913b
Shabahang Saffari <shabahang.saffari@gmail.com>
2024-02-07 21:45:18 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./Sass/index.scss";
+
+function App() {
+  return (
+    <>
+      <div className="light_mode">
+        <h2 className="my_header">Shabahang's Portfolio</h2>
+        <div>
+          <h4>Hello this is the text</h4>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
