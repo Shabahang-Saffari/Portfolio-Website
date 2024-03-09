3fe5dd24bc3ed9ffed705e1dacc088cbc800d017
Shabahang Saffari <shabahang.saffari@gmail.com>
2024-03-08 17:06:09 -0800

    updating the URL for BugTracker

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -159,7 +159,7 @@ export const my_projects = [
     id: nanoid(),
     name: "BugTracker",
     live_url: "https://www.google.com",
-    github_url: "https://github.com/Shabahang-Saffari/BugTracker-Deployed",
+    github_url: "https://bugtracker24.azurewebsites.net/",
     description:
       "This application is a comprehensive full-stack solution, incorporating robust authentication and authorization features. Users can effortlessly sign up, and the admin defines roles and access levels. The application provides a range of functionalities, including project and ticket creation/editing, commenting, and more. Users benefit from a powerful search and filter system, ensuring efficient management and tracking of bugs and issues.",
     techs: [
