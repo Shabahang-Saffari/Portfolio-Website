ede92d5e9b7f7f3f3e7f597cc51b52ef270af8a4
Shabahang Saffari <shabahang.saffari@gmail.com>
2024-08-11 21:56:39 -0700

    editing edu section

--- src/components/About me/AboutMe.jsx
+++ src/components/About me/AboutMe.jsx
@@ -45,14 +45,14 @@ const AboutMe = () => {
             <ul>
               <li>Bachelor of Computer Software Engineering</li>
               <p>May 2012, Islamic Azad University, Tehran, Iran</p>
+              <li>Master of Computer Music</li>
+              <p>January 2019, University of Victoria, Victoria, BC, Canada</p>
               <li>Python Programming (Grade A)</li>
               <p>Fall 2022, BCIT, BC, Canada </p>
               <li>NodeJS</li>
               <p>Udemy Certificate, 2023</p>
               <li>React</li>
               <p>Udemy Certificate, 2024</p>
-              <li>Master of Computer Music</li>
-              <p>January 2019, University of Victoria, Victoria, BC, Canada</p>
             </ul>
           </div>
         </div>
